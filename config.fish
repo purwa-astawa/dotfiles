c25b575294b967c53f254bdaea94ab7fe60d2b2a
Purwa Astawa <purwa@crazyslug.net>
2022-04-15 18:35:31 +0800

    update kitty

---
+++ config.fish
@@ -0,0 +1,5 @@
+set PATH /home/purwa/.local/share/nvm/v16.13.2/bin $PATH
+set PATH /usr/local/go/bin $PATH
+set PATH /home/purwa/.local/kitty.app/bin/kitty $PATH
+set -g fish_term24bit 1
+alias vim="nvim"
